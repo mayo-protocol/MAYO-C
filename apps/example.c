d146078b1e8c9ea7af7021ecf54caee664c60c5e
Satwik Raj Wadhwa <80780252+Satwik-Raj@users.noreply.github.com>
2025-05-12 23:40:59 +0530

    comments in example.c

--- apps/example.c
+++ apps/example.c
@@ -25,24 +25,25 @@
 static int example_mayo(const mayo_params_t* p) {
 
     size_t msglen = 32;
-    size_t smlen = PARAM_sig_bytes(p) + msglen;
+    size_t smlen = PARAM_sig_bytes(p) + msglen;//something(p) gives the specific parameter from the parameter set
 
-    unsigned char *pk  = calloc(PARAM_cpk_bytes(p), 1);
-    unsigned char *sk  = calloc(PARAM_csk_bytes(p), 1);
+    unsigned char *pk  = calloc(PARAM_cpk_bytes(p), 1);//public key
+    unsigned char *sk  = calloc(PARAM_csk_bytes(p), 1);//secret key
 
-    uint64_t *epk = calloc(1, sizeof(pk_t));
-    sk_t *esk = calloc(1, sizeof(sk_t));
+    uint64_t *epk = calloc(1, sizeof(pk_t));//[DOUBT] : why not use pk_t defined in mayo.h, doesn't matter tho
+    sk_t *esk = calloc(1, sizeof(sk_t));//sk_t is a struct defined in mayo.h
+    // an internal representation optimized for fast signing
 
-    unsigned char *sig = calloc(PARAM_sig_bytes(p) + msglen, 1);
+    unsigned char *sig = calloc(PARAM_sig_bytes(p) + msglen, 1);//buffer for signature + message
 
-    unsigned char msg[32] = { 0xe };
-    unsigned char msg2[32] = { 0 };
+    unsigned char msg[32] = { 0xe };//initial message
+    unsigned char msg2[32] = { 0 };//buffer for the recovered message in 
 
-    printf("Example with %s\n", PARAM_name(p));
+    printf("Example with %s\n", PARAM_name(p));//name of the parameter set
 
     printf("mayo_keypair -> ");
     int res = mayo_keypair(p, pk, sk);
-    if (res != MAYO_OK) {
+    if (res != MAYO_OK) {//MAYO_OK is 0
         printf("FAIL\n");
         res = -1;
         goto err;
@@ -50,6 +51,7 @@ static int example_mayo(const mayo_params_t* p) {
         printf("OK\n");
     }
 
+    //expands the compacted secret key (sk) to the expanded secret key (esk)
     printf("mayo_expand_sk -> ");
     res = mayo_expand_sk(p, sk, esk);
     if (res != MAYO_OK) {
@@ -60,6 +62,7 @@ static int example_mayo(const mayo_params_t* p) {
         printf("OK\n");
     }
 
+    //expands the compacted public key (pk) to the expanded public key (epk)
     printf("mayo_expand_pk -> ");
     res = mayo_expand_pk(p, pk, epk);
     if (res != MAYO_OK) {
@@ -79,7 +82,12 @@ static int example_mayo(const mayo_params_t* p) {
     } else {
         printf("OK\n");
     }
-
+    //MAYO_OPEN
+    //The implementation performs Mayo.verify().
+    //If the signature verification succeeded, the recovered message is stored in msg2
+    //&msglen is pointer to the length of message
+    //Keys provided is a compact public key.
+    //The caller is responsible to allocate sufficient memory to hold m.
     printf("mayo_open (with correct signature) -> ");
     res = mayo_open(p, msg2, &msglen, sig, smlen, pk);
     if (res != MAYO_OK || memcmp(msg, msg2, msglen)) {
@@ -90,7 +98,9 @@ static int example_mayo(const mayo_params_t* p) {
         res = MAYO_OK;
         printf("OK\n");
     }
-
+    //MAYO_VERIFY
+    //If the signature verification succeeded, returns 0, otherwise 1.
+    //Key provided is a compact public key.
     printf("mayo_verify (with correct signature) -> ");
     res = mayo_verify(p, msg, msglen, sig, pk);
     if (res != MAYO_OK) {
@@ -103,8 +113,8 @@ static int example_mayo(const mayo_params_t* p) {
     }
 
     printf("mayo_open (with altered signature) -> ");
-    sig[0] = ~sig[0];
-    memset(msg2, 0, msglen);
+    sig[0] = ~sig[0];//alter one bit to simulate corruption
+    memset(msg2, 0, msglen);//reset the buffer in order to be used for new recovered message
     res = mayo_open(p, msg2, &msglen, sig, smlen, pk);
     if (res != MAYO_ERR || !memcmp(msg, msg2, msglen)) {
         printf("FAIL\n");
@@ -129,6 +139,7 @@ static int example_mayo(const mayo_params_t* p) {
 err:
     free(pk);
     free(epk);
+    //secure_free is likely a zeroing + free, to avoid leaking sensitive data
     mayo_secure_free(sk, PARAM_csk_bytes(p));
     mayo_secure_free(esk, sizeof(sk_t));
     free(sig);
